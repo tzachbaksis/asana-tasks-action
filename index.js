e9c508f76bd7b5c3d7d7639eb540a497378dc8d8
Tzach Baksis <tzachbaksis@gmail.com>
2022-01-30 00:19:56 +0200

    Add main logic - index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const asana = require('asana')
+
+const main = async () => {
+  try {
+    const asanaToken = core.getInput('asana_token', { required: true })
+    const pr = github.context.payload.pull_request
+    core.info(
+        `Running action for PR' #${pr.number}: ${pr.title}`,
+    )
+    const client = asana.Client.create().useAccessToken(asanaToken)
+    const me = await client.users.me()
+    core.info(`My name is: ${me.name}`)
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+await main();

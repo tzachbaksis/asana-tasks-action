3b1d606813547bc7e70124daebb98fcaede09b61
Tzach Baksis <tzachbaksis@gmail.com>
2022-02-02 10:57:41 +0200

    debug

--- dist/index.js
+++ dist/index.js
@@ -73124,14 +73124,12 @@ const asana = __nccwpck_require__(3565)
 ASANA_URL = 'https://app.asana.com'
 ASANA_TASK_REGEX = RegExp(`${ASANA_URL}/[0-9]/[0-9]*/([0-9]*)/.*`)
 
-const getAsanaShortIds = (body) => {
-  if (!body) return null
+const extractAsanaTaskIds = (prDescription) => {
+  if (!prDescription) return []
   const taskIds = []
-  const lines = body.split('\n')
-  console.log(lines)
+  const lines = prDescription.split('\n')
   while (lines.length > 0) {
     const line = lines.shift()
-    console.log(line)
     const possibleMatch = ASANA_TASK_REGEX.exec(line)
     if (possibleMatch) {
       taskIds.push(possibleMatch[1])
@@ -73140,27 +73138,24 @@ const getAsanaShortIds = (body) => {
   return taskIds
 }
 
-
 const main = async () => {
   try {
     const asanaToken = core.getInput('asana_token', { required: true })
     const client = asana.Client.create().useAccessToken(asanaToken)
     const pr = github.context.payload.pull_request
     const isMerged = github.context.payload.pull_request.merged
-    core.info(
-        `Running action for PR #${pr.number}: ${pr.body}. Is merged: ${isMerged}.`,
-    )
-    core.info('before')
-    try {
-      const ids = getAsanaShortIds(pr.body)
-      core.info(ids.toString())
-    } catch (e) {
-      console.error(e.payload)
+    core.info(`Running action for PR #${pr.number}: ${pr.body}`)
+    core.info(`Is merged: ${isMerged}`)
+    // if (!isMerged) return
+
+    const taskIds = extractAsanaTaskIds(pr.body)
+    core.info(taskIds.toString())
+    for (const taskId in taskIds) {
+      core.info(`Updating task: ${taskId} to be completed`)
+      await client.tasks.updateTask(taskId, {
+        completed: true
+      })
     }
-
-    const me = await client.users.me()
-    core.info(`My name is: ${me.name}`)
-
   } catch (error) {
     core.setFailed(error.payload);
   }

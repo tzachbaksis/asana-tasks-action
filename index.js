3a8c1ca282ae0f2b76b6ed51ec0613b982202807
Tzach Baksis <tzachbaksis@gmail.com>
2022-02-02 09:31:39 +0200

    debug

--- index.js
+++ index.js
@@ -2,16 +2,40 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const asana = require('asana')
 
+const getAsanaShortIds = (body) => {
+  if (!body) return null
+
+  body = body.replace(/ /g, '') // raw body
+
+  const lines = body.split('\n')
+  while (lines.length > 0) {
+    const line = lines.shift()
+    if (startsWithAnyPrefix(line, commentPrefixes)) {
+      const resp = []
+      let matches
+      const reg = RegExp('https://app.asana.com/[0-9]/[0-9]*/[0-9]*', 'g')
+      while ((matches = reg.exec(line)) !== null) {
+        resp.push(...matches[0].split('/').slice(-1))
+      }
+      return resp
+    }
+  }
+  return []
+}
+
+
 const main = async () => {
   try {
     const asanaToken = core.getInput('asana_token', { required: true })
+    const client = asana.Client.create().useAccessToken(asanaToken)
     const pr = github.context.payload.pull_request
     const isMerged = github.context.payload.pull_request.merged
-    const client = asana.Client.create().useAccessToken(asanaToken)
     core.info(
-        `Running action for PR' #${pr.number}: ${pr.body}`,
+        `Running action for PR #${pr.number}: ${pr.body}. Is merged: ${isMerged}.`,
     )
-    core.info(`is merged: ${isMerged}`)
+    const shortIds = getAsanaShortIds(pr.body)
+    console.log(shortIds)
+
     const me = await client.users.me()
     core.info(`My name is: ${me.name}`)
 
